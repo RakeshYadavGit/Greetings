af3eca7757331004c1521b7a090db696ee08e7dd
Rakesh Yadav <rryadav142001@gmail.com>
2025-11-01 13:54:11 +0530

    initial setup

---
+++ Greetings/Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Rakesh Yadav on 01/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
